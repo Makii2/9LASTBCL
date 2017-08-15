cffa78d26dc37fe5a505f90b26a95bea9c214126
Makii2 <yeisong219@gmail.com>
2017-08-15 17:43:24 -0600

    Add files via upload

---
+++ LABORATORIO 3/Primer Problema de Java/src/codigos/Codigos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigos;
+import java.util.Scanner;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Codigos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    double x;
+    double y;
+    
+    System.out.println("Ingrese segunda cantidad");
+    Scanner D1 = new Scanner (System.in);
+    x = D1.nextInt();
+    
+    y = 15+105;
+    
+    System.out.println(y);
+    
+    }
+    
+}
---
+++ LABORATORIO 3/Segundo Nombres/src/segundo/nombres/SegundoNombres.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundo.nombres;
+import java.util.Scanner;
+/**
+ *
+ * @author estudiante
+ */
+public class SegundoNombres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String F;
+    String C;
+    
+    System.out.println("Buen Dia");
+    Scanner D1 = new Scanner(System.in);
+    C = D1.nextInt();
+    
+    String F = Eduardo("Buen Dia");
+    
+    System.out.println(F);
+    
+    
+    
+    
+    }
+    
+}
